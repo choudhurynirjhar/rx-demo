2b560b27f4ad3fb109dbf18f85f1d67c580f52b2
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2021-02-21 22:59:32 -0500

    First checkin

---
+++ Rx.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Rx.Demo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable.Range(1, 100).Where(x => x % 2 == 0)
+                .Merge(Observable.Range(1, 100).Where(x => x % 2 == 0))
+                .Subscribe(Console.WriteLine);
+
+            Console.WriteLine("Completed!");
+            Console.ReadLine();
+        }
+    }
+}
